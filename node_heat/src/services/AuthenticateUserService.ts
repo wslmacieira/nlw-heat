4b4759aeeca41d5ac69298826a2771a2e4e8d42e
wslmacieira <wslmacieira@outlook.com>
2021-10-22 18:52:59 -0300

    Autenticação de usuário recebendo o código

---
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { sign } from "jsonwebtoken";
+import { prismaClient } from "../prisma";
+/*
+  Receber o code(string)
+  Recuperar o access_token no github
+  Recuperar infos do user no github
+  Verificar se o usuário existe
+  ---Sim = Gera um token
+  ---Não = Cria no DB, gera um token
+  Retornar o token com as infos do usuário logado.
+*/
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
+export class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          Authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    const { avatar_url, id, login, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+
+      const token = sign(
+        {
+          user: {
+            id: user.id,
+            name: user.name,
+            avatar_url: user.avatar_url,
+          },
+        },
+        process.env.JWT_SECRET as string,
+        {
+          subject: user.id,
+          expiresIn: "1d",
+        }
+      );
+      return { token, user };
+    }
+  }
+}
