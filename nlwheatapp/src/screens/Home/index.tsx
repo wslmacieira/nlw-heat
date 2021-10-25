430c317e8cbac4d92018debfb30d15fede9682fa
wslmacieira <wslmacieira@outlook.com>
2021-10-25 14:00:43 -0300

    Cria Header e UserPhot

--- nlwheatapp/src/screens/Home/index.tsx
+++ nlwheatapp/src/screens/Home/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Header } from '../../components/Header';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Olá React Native</Text>
+      <Header />
     </View>
   );
 }
