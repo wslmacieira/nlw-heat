23a5d56146a957d8eea325027d13d745d568625e
wslmacieira <wslmacieira@outlook.com>
2021-10-26 19:12:37 -0300

    finalizando os componentes

--- nlwheatapp/src/screens/Home/index.tsx
+++ nlwheatapp/src/screens/Home/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { SignInBox } from '../../components/SignInBox';
+import { SendMessageForm } from '../../components/SendMessageForm';
 
 import { styles } from './styles';
 
@@ -10,6 +12,7 @@ export function Home() {
     <View style={styles.container}>
       <Header />
       <MessageList/>
+      <SendMessageForm />
     </View>
   );
 }
